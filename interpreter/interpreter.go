8e2d168c25a90058d594948c1bf2ee49ce658f50
Faisal Alatawi <fa.atawi@gmail.com>
2020-03-03 01:10:19 -0700

    inter

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -2,42 +2,112 @@ package interpreter
 
 import (
 	"GoLox/ast"
+	"GoLox/token"
+	"errors"
 	"fmt"
 )
 
-// LoxInterpreter is an empty struct
-type LoxInterpreter struct{}
+func Interpret(e ast.Expr) {
+	value, err := evaluate(e)
+	if err != nil {
+		// do something
+	}
+	fmt.Println(value)
+}
 
-func Interpret(e ast.Expr) string {
+func evaluate(e ast.Expr) (interface{}, error) {
 	switch t := e.(type) {
 
 	case ast.Binary:
-		exprs := []ast.Expr{t.left, t.right}
-		return parenthesize(t.operator.Lexeme, exprs)
+		return evaluateBinary(t)
 
 	case ast.Grouping:
-		exprs := []Expr{t.expression}
-		return parenthesize("group", exprs)
+		return evaluate(t.Expression)
 
 	case ast.Literal:
-		switch t.value.(type) {
-		case nil:
-			return "nil"
-		case string:
-			s, _ := t.value.(string)
-			return s
-		case float64, int, float32:
-			f, _ := t.value.(float64)
-			return fmt.Sprintf("%f", f)
-		default:
-			return "error"
-		}
+		return t.Value, nil
 
 	case ast.Unary:
-		exprs := []Expr{t.right}
-		return parenthesize(t.operator.Lexeme, exprs)
+		right, err := evaluate(t.Right)
+		if err != nil {
+			// do something
+		}
+
+		switch t.Operator.Type {
+		case token.MINUS:
+			num, ok := right.(float64)
+			if !ok {
+				return nil, errors.New("sksk")
+			}
+			return -1 * num, nil
+
+		case token.BANG:
+			return !isTruthy(right), nil
+
+		}
+		return nil, nil
 
 	default:
-		return "error"
+		return "error", errors.New("sksk")
 	}
+}
+
+func evaluateBinary(b ast.Binary) (interface{}, error) {
+	left, errL := evaluate(b.Left)
+	right, errR := evaluate(b.Right)
+
+	if errL != nil || errR != nil {
+		return nil, errors.New("sksk")
+	}
+
+	switch b.Operator.Type {
+	case token.MINUS:
+		numL, numR, err := checkNumberOperands(left, right)
+		if err != nil {
+			return nil, errors.New("TODO")
+		}
+		return numL - numR, nil
+
+	case token.SLASH:
+		numL, numR, err := checkNumberOperands(left, right)
+		if err != nil {
+			return nil, errors.New("TODO")
+		}
+		return numL / numR, nil
+
+	case token.STAR:
+		numL, numR, err := checkNumberOperands(left, right)
+		if err != nil {
+			return nil, errors.New("TODO")
+		}
+		return numL * numR, nil
+
+	case token.PLUS:
+
+	}
+	return nil, nil
+}
+
+func checkNumberOperands(left, right interface{}) (float64, float64, error) {
+	numL, okL := left.(float64)
+	numR, okR := left.(float64)
+
+	if okL && okR {
+		return numL, numR, nil
+	} else {
+		return 0.0, 0.0, errors.New("TODO")
+	}
+
+}
+
+func isTruthy(e ast.Expr) bool {
+	if e == nil {
+		return false
+	}
+
+	if t, ok := e.(bool); ok {
+		return t
+	}
+
+	return true
 }
