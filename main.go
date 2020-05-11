18fcc1775d4683a0f52203915e23605d6507e01d
Faisal Alatawi <fa.atawi@gmail.com>
2020-05-11 14:05:50 -0700

    go

--- main.go
+++ main.go
@@ -6,19 +6,15 @@
 
 package main
 
-<<<<<<< HEAD
-import "GoLox/scanner"
-=======
-import "GoLox/interpreter"
->>>>>>> 4850e791c5032df6a826136a1a8c12d2af84058f
+import (
+	"GoLox/interpreter"
+	"GoLox/scanner"
+)
 
 func main() {
 	scanner.Test()
 	// parser.Test()
 	// ast.Test()
-<<<<<<< HEAD
-=======
 	interpreter.Test()
->>>>>>> 4850e791c5032df6a826136a1a8c12d2af84058f
 
 }
